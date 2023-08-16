bc7566c28acf303181aa43e4498c023d6ec883b8
FlynnHillier <flynnhillier@gmail.com>
2023-08-16 14:30:58 +0100

    added timestep to get_all_faces

--- faces.py
+++ faces.py
@@ -9,42 +9,50 @@ import cv2
 # convert frame to greyscale to improve accuracy (convert back when saving image)
 
 #return list of numpy arrays
-def get_all_faces(video_filepath:str) -> list[any]: 
+def get_all_faces(video_filepath:str,time_step:int = 1) -> list[any]: 
     """"""
     cap = cv2.VideoCapture(video_filepath)
 
-    #if failed to load video
+    # if failed to load video
     if not cap.isOpened():
         raise Exception(f"failed to open video {video_filepath}")
     
+    
+    #calculate frame step
+    cap_fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_step = int(time_step * cap_fps)
+
+
     distinctive_face_encodings = []
     distinctive_face_images = []
 
-    #itterate through each frame of video and check for new faces
+    # itterate through each frame of video and check for new faces
     while cap.isOpened():
         success,frame = cap.read()
 
         if not success:
             break
-        
-        #fetch face-recognition information about the current frame
+
+        if not cap.get(cv2.CAP_PROP_POS_FRAMES) % frame_step == 0:
+            continue
+
+        # fetch face-recognition information about the current frame
         frame_face_locations = face_recognition.face_locations(img=frame)
         frame_face_encodings = face_recognition.face_encodings(face_image=frame,known_face_locations=frame_face_locations)
 
         for i,face_encoding in enumerate(frame_face_encodings):
 
-            #compare face-encodings within current frame, against already identified encodings
+            # compare face-encodings within current frame, against already identified encodings
             matches = face_recognition.compare_faces(distinctive_face_encodings,face_encoding)
             
             if not True in matches:
-                #face encoding is new and distinctive from previous
+                # face encoding is new and distinctive from previous
                 distinctive_face_encodings.append(face_encoding)
 
-                #crop frame to only face
+                # crop frame (colored) to only face
                 y1,x2,y2,x1 = frame_face_locations[i]
                 frame_cropped_to_face = frame[y1:y2,x1:x2]
                 distinctive_face_images.append(frame_cropped_to_face)
-
 
     return (distinctive_face_encodings,distinctive_face_images)
 
